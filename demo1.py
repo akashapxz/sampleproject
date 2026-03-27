d2de38a4105ecc522ca8ff2a323670cbe0116932
Akash <akashjp55555@gmail.com>
2026-03-27 14:38:25 +0530

    Added positive/negative check feature

--- demo1.py
+++ demo1.py
@@ -4,3 +4,11 @@ if num % 2 == 0:
     print("Even number")
 else:
     print("Odd number")
+
+# New feature added
+if num > 0:
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero")
